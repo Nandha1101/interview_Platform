6fe3a5988fa84d9d39c99526bc1c2f55293ff617
[Nandha Kishor] <[nandhakishor507@gmail.com]>
2026-02-12 01:25:52 +0530

    server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
 import { ENV } from "./lib/env.js";
+import chatRoutes from "./routes/chatRoutes.js"
 
 const app = express();
 
@@ -14,17 +16,21 @@ app.use(cors({
   origin: ENV.CLIENT_URL,
   credentials: true
 }));
+app.use(clerkMiddleware()); //this adds auth field to request object
 
 // Inngest endpoint
 app.use(
   "/api/inngest",
   serve({ client: inngest, functions })
 );
+app.use("/api/chat",chatRoutes)
 
 // Test route
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Success from api" });
 });
+
+
 
 // ✅ SINGLE server start
 const PORT = process.env.PORT || ENV.PORT || 3000;
