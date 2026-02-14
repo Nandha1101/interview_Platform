b27a76e7bdbf4a63cc812403b18238dde9724edc
[Nandha Kishor] <[nandhakishor507@gmail.com]>
2026-02-15 03:16:06 +0530

    Refractoring sessionController.js

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -99,6 +99,10 @@ export async function joinSession(req,res) {
        
        if(!session) return res.status(404).json({message:"Session not found"});
 
+       if(!session.host.toString() === userId.toString()){
+        return res.status(400).json({message:"Host cannot join their own session as participant"});
+       }
+
     //check if session is already full - has a participant
     if(session.participant) return res.status(404).json({message:"Session is full"})
 
@@ -133,8 +137,6 @@ try{
         return res.status(400).json({message:"Session is already comleted "});
        }
 
-       session.status="completed"
-       await session.save()
 
        //delete stream video call
        const call = streamClient.video.call("default",session.callId)
@@ -143,6 +145,10 @@ try{
        //delete stream chat channel
        const channel = chatClient.channel("messaging",session.callId);
        await channel.delete();
+
+       
+       session.status="completed";
+       await session.save();
 
        res.status(200).json({session,message:"Session ended successfully"});
 
