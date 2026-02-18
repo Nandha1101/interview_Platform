523646a53460eb07c866773252dcbfd1241296fa
[Nandha Kishor] <[nandhakishor507@gmail.com]>
2026-02-19 00:05:05 +0530

    Landing Page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,21 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProblemsPage from './pages/ProblemsPage';
 import {Toaster} from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
 function App() {
-    const { isSignedIn } = useUser();
+    const { isSignedIn ,isLoaded} = useUser();
      
+    // this will get rid of flickering effect
+    if (!isLoaded) return null;
+    
   return (
     <>
     
     <Routes>
      <Route path="/about" element={<AboutPage/>}/>
-     <Route path="/" element={<HomePage/>}/>
+     <Route path="/" element={!isSignedIn ? <HomePage/> : <Navigate to={"/dashboard"}/>}/>
+     <Route path="/dashboard" element={isSignedIn ? <DashboardPage/> : <Navigate to={"/"}/>}/>
+     
      <Route path="/problems" element={isSignedIn ? <ProblemsPage/> : <Navigate to ={"/"}/>}/>
     </Routes>
     <Toaster toastOptions={{ duration : 3000 }}/>
